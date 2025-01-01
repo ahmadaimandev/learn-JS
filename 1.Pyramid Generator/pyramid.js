8f319824e9137a7c4a38505577ed85a771daf6b4
Aiman <aimanzulbahari@gmail.com>
2025-01-01 16:20:52 +0800

    - Add new note about array

--- 1.Pyramid Generator/pyramid.js
+++ 1.Pyramid Generator/pyramid.js
@@ -5,8 +5,13 @@ let profession = "Teacher";
 let count = 8; 
 
 // Perform mathematical operations
-console.log("the output is: " + count + 1); // 9
-console.log(count - 1); // 7
-console.log("the output is: " + count * 2); // 16
-console.log("the output is: " + count / 2); // 45
-console.log("the output is: " + count % 2); // 0
+// console.log("the output is: " + count + 1); // 9
+// console.log(count - 1); // 7
+// console.log("the output is: " + count * 2); // 16
+// console.log("the output is: " + count / 2); // 45
+// console.log("the output is: " + count % 2); // 0
+
+// declaring an array
+// declare strings values in an array
+let rows = ["Naomi", "Quincy", "CamperChan"];  
+console.log(rows[0]);
