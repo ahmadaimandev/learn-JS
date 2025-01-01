94acc674ebe37922f6f893297e15c3263136c0b7
Aiman <aimanzulbahari@gmail.com>
2025-01-01 20:54:45 +0800

    - sign off. will continue lesson later

--- 1.Pyramid Generator/pyramid.js
+++ 1.Pyramid Generator/pyramid.js
@@ -1,8 +1,8 @@
 let profession = "Teacher";
 let count = 8; 
 let rows = ["Naomi", "Quincy", "CamperChan"];  
-rows.push("freeCodeCamp"); // add new element to the array
+let pushed = rows.push("freeCodeCamp"); // add new element to the array
 let popped = rows.pop()
 console.log(popped);
 console.log(rows);
-
+console.log(pushed);
