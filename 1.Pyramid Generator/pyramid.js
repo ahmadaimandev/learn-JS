282f2337d7d1ca70e45d300e1b5359d4468227ca
Aiman <aimanzulbahari@gmail.com>
2025-01-01 12:56:52 +0800

    Update file pyramid.js

--- 1.Pyramid Generator/pyramid.js
+++ 1.Pyramid Generator/pyramid.js
@@ -3,3 +3,9 @@ console.log(character);
 // Reassign value of character
 character = 'World';
 console.log(character);
+
+// camel case
+// when declare variable without value, it is considered uninitialized.
+let secondCharater; 
+secondCharater = character;
+console.log(secondCharater); // output will show: undefined. it is default value
