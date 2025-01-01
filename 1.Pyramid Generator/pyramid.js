949215f4d830ad19ea67be1d98170387fd2a425d
Aiman <aimanzulbahari@gmail.com>
2025-01-01 20:47:59 +0800

    - update code file

--- 1.Pyramid Generator/pyramid.js
+++ 1.Pyramid Generator/pyramid.js
@@ -1,21 +1,8 @@
 let profession = "Teacher";
-
-// declare number
-// do not use quotes
 let count = 8; 
-
-// Perform mathematical operations
-// console.log("the output is: " + count + 1); // 9
-// console.log(count - 1); // 7
-// console.log("the output is: " + count * 2); // 16
-// console.log("the output is: " + count / 2); // 45
-// console.log("the output is: " + count % 2); // 0
-
-// declaring an array
-// declare strings values in an array
 let rows = ["Naomi", "Quincy", "CamperChan"];  
-// console.log(rows[0]);
-// // change third element in the array
-// // rows[2] = 10;
-// rows[rows.length - 1]; //will return the number of elements in the array 
+rows.push("freeCodeCamp"); // add new element to the array
+let popped = rows.pop()
+console.log(popped);
 console.log(rows);
+
