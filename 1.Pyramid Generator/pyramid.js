86d53a977b1397e5210245011c2351dd2c13cf34
Aiman <aimanzulbahari@gmail.com>
2025-01-03 22:00:38 +0800

    - Add new note file

--- 1.Pyramid Generator/pyramid.js
+++ 1.Pyramid Generator/pyramid.js
@@ -1,8 +1,3 @@
-let profession = "Teacher";
+let profession = "Hello";
 let count = 8; 
-let rows = ["Naomi", "Quincy", "CamperChan"];  
-let pushed = rows.push("freeCodeCamp"); // add new element to the array
-let popped = rows.pop()
-console.log(popped);
-console.log(rows);
-console.log(pushed);
+let rows = [];  
