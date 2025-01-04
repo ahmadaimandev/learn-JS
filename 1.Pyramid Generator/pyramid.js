a0bcd9d34f5d6e957a4e025257eec1ee63a11ee4
Aiman <aimanzulbahari@gmail.com>
2025-01-04 21:56:43 +0800

    add loopnote.txt with for loop examples and update pyramid.js to include loop structure

--- 1.Pyramid Generator/pyramid.js
+++ 1.Pyramid Generator/pyramid.js
@@ -1,3 +1,8 @@
 const character = "#"; //will serve as building block for the pyramid
 const count = 8;
 const rows = [];
+
+//create loop using 'for' loop
+
+for(let i = 0; "condition"; "iteration") {
+}
