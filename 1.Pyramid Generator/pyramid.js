7183835fc16deb235f6c0e6de3ff4be0742bba76
Aiman <aimanzulbahari@gmail.com>
2025-01-05 17:44:00 +0800

    create new loop

--- 1.Pyramid Generator/pyramid.js
+++ 1.Pyramid Generator/pyramid.js
@@ -4,5 +4,15 @@ const rows = [];
 
 //create loop using 'for' loop
 
-for(let i = 0; "condition"; "iteration") {
+for(let i = 0; i < count; i = i + 1) {
+    rows.push(i);
 }
+
+let result = "";
+
+//create for....of loop
+for(const row of rows){
+    result = result + character;
+    console.log(result);
+}
+console.log(result);
